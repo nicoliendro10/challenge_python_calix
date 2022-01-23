76cdf8568b2bc920cc9ccc6c8251d9ab4cc7d65f
nicoliendro10 <nick.sfra.7@gmail.com>
2022-01-23 20:51:04 -0300

    Update normalize_data_service.py
    Refactor names of variables

--- src/normalize_data_service.py
+++ src/normalize_data_service.py
@@ -9,13 +9,13 @@ class NormalizeDataService:
 
     def normalize_headers(self, columns):
         """ Normalize the headers of the dataframe, converting all the headers to lowercase and replacing special characters """
-        new_data_column = []
-        for x in columns:
-            x = x.lower()
-            new_column = re.sub(
+        converted_data_column = []
+        for column in columns:
+            column = column.lower()
+            column_normalized = re.sub(
                 r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1",
-                normalize("NFD", x), 0, re.I
+                normalize("NFD", column), 0, re.I
             )
-            new_column = normalize('NFC', new_column)
-            new_data_column.append(new_column)
-        return new_data_column
+            column_normalized = normalize('NFC', column_normalized)
+            converted_data_column.append(column_normalized)
+        return converted_data_column
