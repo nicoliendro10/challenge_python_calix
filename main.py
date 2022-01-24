75f942f136c7bdfa11612dd1c6625f33a4a69877
nicoliendro10 <nick.sfra.7@gmail.com>
2022-01-24 11:15:43 -0300

    Update main.py
    Add functionality for the second part of the challenge. Creating dfs and normalizing data.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from input_files_service import InputFilesService
-from normalize_data_service import NormalizeDataService
-from sql_service import SQLService
+from src.input_files_service import InputFilesService
+from src.normalize_data_service import NormalizeDataService
+from src.sql_service import SQLService
 
+normalize_data_service = NormalizeDataService()
 filename_path_list = []
+
+
 
 def get_input_files(urls):
     """ Get the input files from the url and save them in the correct directory """
@@ -21,28 +24,40 @@ def get_input_files(urls):
         input_files_service.create_directory(file_name)
         input_files_service.save_input_file(response, file_name)
 
-def normalize_input_files():
+
+def normalize_partial_input_files():
     """ Normalize the input files """
-    normalize_data_service = NormalizeDataService()
     list_dataframes = []
     for filename_path in filename_path_list:
         df = normalize_data_service.read_csv(filename_path)
-        converted_data_column = normalize_data_service.normalize_headers(df.columns)
+        converted_data_column = normalize_data_service.normalize_headers(
+            df.columns)
         df.columns = converted_data_column
         list_dataframes.append(df)
-    dataframes_joined = normalize_data_service.normalize_dataframes(list_dataframes)
-    #Insert this to the database
-    dataframes_with_setted_columns = normalize_data_service.set_columns(dataframes_joined)
-    sql_service = SqlService()
-    
+    return list_dataframes
+
+def get_data_by_criteria(unified_table):
+    """ Get the data by criteria """
+    matrix_criteria = [['categoria'], ['fuente'], ['idprovincia', 'categoria']]
+    table_by_criteria = []
+    for list_criteria in matrix_criteria:
+        df_by_criteria = normalize_data_service.get_df_by_criteria(unified_table, list_criteria)
+        table_by_criteria.append(df_by_criteria)
+    normalize_data_service.normalize_dataframes(table_by_criteria)
+    return normalize_data_service
+
 
 
 if __name__ == '__main__':
-    sql_service = SQLService()
-    sql_service.config_credentials()
     urls = ['https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_4207def0-2ff7-41d5-9095-d42ae8207a5d',
             'https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_392ce1a8-ef11-4776-b280-6f1c7fae16ae',
             'https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_01c6c048-dbeb-44e0-8efa-6944f73715d7'
             ]
     get_input_files(urls)
+    list_dataframes = normalize_partial_input_files()
+    normalize_data_service = NormalizeDataService()
+    #First table
+    unified_table = normalize_data_service.normalize_dataframes(list_dataframes)
+    #Second table
+    data_by_criteria = get_data_by_criteria(unified_table)
 
